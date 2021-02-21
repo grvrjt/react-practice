63d3bf6fc3023f897793a0782cca49b92144badb
grvrjt <gaurav1993rajput@gmail.com>
2021-02-21 23:33:44 +0530

    feat:4 options of event binding

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,12 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import FunctionClick from './components/FunctionClick';
 import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <EventBind />
        <FunctionClick />
        <ClassClick />
       </header>
